7112ed59784dda0bafc09d84fa16b8ebfb1b3c70
sergak <dog.vol.serg@gmail.com>
2022-02-16 16:06:32 +0300

    describe syntax, add parser.py

---
+++ expression.py
@@ -0,0 +1,7 @@
+
+class ExpressionParser:
+    def __init__(self):
+        pass
+
+    def __call__(self, s, variables):
+        return 1
--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import re
 
-
-def parse_func(s):
-    name = s[:s.find('(')]
-    if s.find('(') == -1 or s.find(')') == -1:
-        print('Error, there is an error in brace')
-
-    variables = s[s.find('(') + 1:s.rfind(')')].split(',')
-    if s[-1] != '{':
-        print('Error, there is no {')
-    return name, variables
 
 
 if __name__ == '__main__':
