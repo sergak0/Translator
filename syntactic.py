a66cdddecc1c7f6ed1a1428822400ee457a65cbb
chervovn04 <chervovn04@gmail.com>
2022-02-26 12:25:36 +0300

    Determined every function in syntactic.py except Exp. Didn't make any tests (I can't without Exp). Also changed language.txt there were mistakes.

--- syntactic.py
+++ syntactic.py
@@ -2,7 +2,12 @@ from lexical import *
 import ctypes
 
 
-tokens = []
+tokens = []  # List of lexical/Token
+
+
+def CheckToken(idx, token):
+    if idx == len(tokens) or tokens[idx] != token:
+        raise Exception('Expect ' + token)
 
 
 def Program(idx):
@@ -10,10 +15,9 @@ def Program(idx):
         idx = Func(idx)
         if idx != len(tokens):
             idx = Program(idx)
-    else:  # <Definition><Program> | <Definition>
+    else:  # <Definition>;<Program> | <Definition>;
         idx = Definition(idx)
-        if idx == len(tokens) and tokens[idx] != ';':
-            raise Exception('Excepted ;')
+        CheckToken(idx, ';')
         idx += 1
         if idx != len(tokens):
             idx = Program(idx)
@@ -21,55 +25,164 @@ def Program(idx):
 
 
 def Func(idx):
+    CheckToken(idx, 'fn')
+    idx += 1
+    idx = Type(idx)
+    idx = Name(idx)
+    if idx == len(tokens) or tokens[idx] != '(':
+        raise Exception('Expect (')
+    idx += 1
+    if idx == len(tokens) or tokens[idx] != ')':
+        raise Exception('Expect )')
+    idx += 1
+    idx = Block(idx)
     return idx
 
 
-def EType(idx):
+def Type(idx):
+    if idx == len(tokens):
+        raise Exception('Expect variable type')
+    if tokens[idx] != 'int' and tokens[idx] != 'double' and tokens[idx] != 'string':
+        raise Exception('Unknown type encountered')
+    idx += 1
+    while idx != len(tokens) and tokens[idx] == '[':
+        idx += 1
+        CheckToken(idx, ']')
+        idx += 1
     return idx
 
 
 def Name(idx):
+    if idx == len(tokens) or tokens[idx].type != 'name':
+        raise Exception('Expect variable name')
+    idx += 1
     return idx
 
 
 def Params(idx):
+    if idx == len(tokens):
+        raise Exception('Expect params list')
+    idx = Type(idx)
+    idx = Name(idx)
+    while idx != len(tokens) and tokens[idx] == ',':
+        idx += 1
+        idx = Params(idx)
     return idx
 
 
 def Block(idx):
+    CheckToken(idx, '{')
+    idx += 1
+    while True:
+        if idx == len(tokens):
+            raise Exception('Expect }')
+        if idx == '}':
+            idx += 1
+            break
+        idx = Operator(idx)
     return idx
 
 
 def Operator(idx):
+    if idx == len(tokens):
+        raise Exception('Expect operator definition')
+    if tokens[idx] == 'for':
+        idx = For(idx)
+    elif tokens[idx] == 'while':
+        idx = While(idx)
+    elif tokens[idx] == 'if':
+        idx = If(idx)
+    elif tokens[idx] == 'return':
+        idx = Return(idx)
+    elif tokens[idx] == 'int' or tokens[idx] == 'double' or tokens[idx] == 'string':
+        idx = Definition(idx)
+        CheckToken(idx, ';')
+        idx += 1
+    else:
+        idx = Enumeration(idx)
+        CheckToken(idx, ';')
+        idx += 1
     return idx
 
 
 def For(idx):
+    CheckToken(idx, 'for')
+    idx += 1
+    CheckToken(idx, '(')
+    idx += 1
+    idx = Definition(idx)
+    CheckToken(idx, ';')
+    idx += 1
+    idx = Exp(idx)
+    CheckToken(idx, ';')
+    idx += 1
+    idx = Exp(idx)
+    CheckToken(idx, ')')
+    idx += 1
+    idx = Block(idx)
     return idx
 
 
 def Definition(idx):
+    idx = Type(idx)
+    idx = Name(idx)
+    if idx != len(tokens) and tokens[idx] == '=':
+        idx += 1
+        idx = Exp(idx)
+    while idx != len(tokens) and tokens[idx] == ',':
+        idx += 1
+        idx = Name(idx)
+        if idx != len(tokens) and tokens[idx] == '=':
+            idx += 1
+            idx = Exp(idx)
     return idx
 
 
 def If(idx):
+    CheckToken(idx, 'if')
+    idx += 1
+    CheckToken(idx, '(')
+    idx += 1
+    idx = Exp(idx)
+    CheckToken(idx, ')')
+    idx += 1
+    idx = Block(idx)
+    if idx != len(tokens) and tokens[idx] == 'else':
+        idx += 1
+        idx = Block(idx)
     return idx
 
 
 def While(idx):
+    CheckToken(idx, 'while')
+    idx += 1
+    CheckToken(idx, '(')
+    idx += 1
+    idx = Exp(idx)
+    CheckToken(idx, ')')
+    idx += 1
+    idx = Block(idx)
     return idx
 
 
 def Return(idx):
-    return idx
-
-
-def Exp(idx):
+    CheckToken(idx, 'return')
+    idx = Exp(idx)
     return idx
 
 
 def Enumeration(idx):
+    idx = Exp(idx)
+    while idx != len(tokens) and idx == ',':
+        idx += 1
+        idx = Exp(idx)
     return idx
+
+
+def Exp(idx):
+    # TODO
+    return idx
+
 
 
 
